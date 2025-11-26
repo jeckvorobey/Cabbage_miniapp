6e4c0a1685097b25adb54cbd916bdef6588a059d
sergei  V <jeckvorobey69@gmail.com>
2025-11-26 11:41:17 +0300

    edit AddUnitModal.vue

--- src/components/AddUnitModal.vue
+++ src/components/AddUnitModal.vue
@@ -3,13 +3,25 @@
     <q-card class="add-product">
       <q-form greedy>
         <q-card-section>
-          <div class="text-h6 q-mb-md">Добавление единицы измерения</div>
-          <q-input v-model="unit.name" outlined class="q-mb-xs" label="Наименование (пример: кг, гр, шт)" />
-          <q-input v-model="unit.symbol" outlined class="q-mb-xs" label="Символ (пример: kg, gr, sh)" />
+          <div class="text-h6 q-mb-md">
+            {{ unit.id ? 'Редактирование' : 'Добавление' }} единицы измерения
+          </div>
+          <q-input
+            v-model="unit.name"
+            class="q-mb-xs"
+            label="Наименование (пример: кг, гр, шт)"
+            outlined
+          />
+          <q-input
+            v-model="unit.symbol"
+            class="q-mb-xs"
+            label="Символ (пример: kg, gr, sh)"
+            outlined
+          />
         </q-card-section>
         <q-card-actions align="right">
-          <q-btn v-close-popup color="red" label="Отмена" />
-          <q-btn v-close-popup color="primary" label="Сохранить" @click="addProduct()"/>
+          <q-btn v-close-popup color="red" flat label="Отмена" />
+          <q-btn v-close-popup color="primary" flat label="Сохранить" @click="addProduct()" />
         </q-card-actions>
       </q-form>
     </q-card>
@@ -17,21 +29,32 @@
 </template>
 
 <script lang="ts" setup>
-  import { useQuasar } from 'quasar';
-  import { useUnitsStore } from 'src/stores/unitsStore';
-  import { ref } from 'vue';
-  import type { IUnit } from 'src/types/unit.interface';
+import { useQuasar } from 'quasar';
+import { useUnitsStore } from 'src/stores/unitsStore';
+import { ref } from 'vue';
+import type { IUnit } from 'src/types/unit.interface';
 
-  const $q = useQuasar();
-  const unitsStore = useUnitsStore()
-  const showDialog = ref(false)
-  const unit = ref<IUnit>({
+interface Props {
+  unit?: IUnit;
+}
+interface Emits {
+  (e: 'update'): void;
+}
+const props = withDefaults(defineProps<Props>(), {
+  unitData: () => ({
     name: '',
     symbol: '',
-  })
+  }),
+});
+const emit = defineEmits<Emits>();
+const $q = useQuasar();
+const showDialog = ref(false);
+const unitsStore = useUnitsStore();
+const unit = ref<IUnit>(props.unitData);
 
-  async function addProduct() {
-    try {
+async function addProduct() {
+  try {
+    if (!unit.value?.id) {
       const res = await unitsStore.createUnit(unit.value);
       if (res) {
         $q.notify({
@@ -39,11 +62,22 @@
           color: 'primary',
         });
       }
-    } catch (e) {
-      console.error(e);
+    } else {
+      const res = await unitsStore.updateUnit(unit.value.id, unit.value);
+      if (res) {
+        $q.notify({
+          message: `Единица измерения успешно обновлена`,
+          color: 'primary',
+        });
+      }
     }
+  } catch (e) {
+    console.error(e);
+  } finally {
+    showDialog.value = false;
+    emit('update');
   }
-
+}
 </script>
 
 <style scoped lang="scss">
