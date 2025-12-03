f7acac17f93ff04261aea1613b657cfb6a509e7d
Denver65 <Denver65@users.noreply.github.com>
2025-12-03 13:23:06 +0000

    deploy: a3802f6241cd69b66345d709de8236f774bb2d03

---
+++ assets/use-quasar-Cbb_mpbr.js
@@ -0,0 +1,1 @@
+import{a,bu as r}from"./index-DtvQfUoh.js";function s(){return a(r)}export{s as u};
--- assets/use-quasar-DHz9wDzy.js
+++
@@ -1,1 +0,0 @@
-import{a,bt as r}from"./index-C5OPW76n.js";function t(){return a(r)}export{t as u};
