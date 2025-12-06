93562c968a9dc3b76d3039b41543a5dc345e4e1b
jeckvorobey <jeckvorobey@users.noreply.github.com>
2025-12-06 19:01:20 +0000

    deploy: 8d5e76ab5dc18de7476f9a5fcf853c31c1afc19e

---
+++ assets/use-quasar-DsYBrZBl.js
@@ -0,0 +1,1 @@
+import{a,bu as r}from"./index-cVLKc0Rk.js";function s(){return a(r)}export{s as u};
--- assets/use-quasar-bOOrUz-9.js
+++
@@ -1,1 +0,0 @@
-import{a,bu as r}from"./index-NZAdI2tp.js";function s(){return a(r)}export{s as u};
