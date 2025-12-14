01c2653c387529fff5616373c6e4232eb532b312
Denver65 <Denver65@users.noreply.github.com>
2025-12-14 23:50:26 +0000

    deploy: 1e8fbff0dc5f433c947b0e8b9ad40b15c6283711

---
+++ assets/axios-ChIHA3Sg.js
@@ -0,0 +1,1 @@
+import{d as a,a as i,c as s}from"./index-D4HhW3th.js";const t=a(({app:o})=>{o.config.globalProperties.$axios=i,o.config.globalProperties.$api=s});export{t as default};
---
+++ assets/yandex-maps-CHtiSwrZ.js
@@ -0,0 +1,1 @@
+import{d as a}from"./index-D4HhW3th.js";import"./vue-yandex-maps-PA5S1kjW.js";const p=a(({app:o})=>{{console.warn("[YandexMaps] - VITE_YANDEX_MAPS_API_KEY не установлен в переменных окружения");return}});export{p as default};
