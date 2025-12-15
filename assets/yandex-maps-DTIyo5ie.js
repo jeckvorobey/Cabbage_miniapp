13004d38ae727f6d4bca625bc59aec582d5fa799
jeckvorobey <jeckvorobey@users.noreply.github.com>
2025-12-15 09:12:56 +0000

    deploy: 01ebaf3d88519f8013c6ab2277f66ba9f173503f

--- assets/yandex-maps-DTIyo5ie.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./index-Cl7R4nCu.js";import"./vue-yandex-maps-C0kiqY2U.js";const p=a(({app:o})=>{{console.warn("[YandexMaps] - VITE_YANDEX_MAPS_API_KEY не установлен в переменных окружения");return}});export{p as default};
