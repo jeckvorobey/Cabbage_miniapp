c8a88cd3de43b6a7bfc828054cd7eac88709c5e4
Denver65 <Denver65@users.noreply.github.com>
2025-12-15 16:36:16 +0000

    deploy: 59e79ce91a4e34c02cff11a9ff084b2277353d90

---
+++ assets/yandex-maps-CX8-zqje.js
@@ -0,0 +1,1 @@
+import{d as a}from"./index-cFVlmZh7.js";import"./vue-yandex-maps-DN_ciqgm.js";const p=a(({app:o})=>{{console.warn("[YandexMaps] - VITE_YANDEX_MAPS_API_KEY не установлен в переменных окружения");return}});export{p as default};
